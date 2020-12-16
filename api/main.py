c64c2d20bd1add91de2edbdb00905b2ab35c3910
zhufue <499984532@qq.com>
2020-12-17 05:44:33 +0800

    提交了文件

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
 
-def main_handler(event, context):
+  def main_handler(event, context):
     r = requests.post("https://api.github.com/repos/Zfour/my-blog-source-file/dispatches",
     json = {"event_type": "run-it"},
     headers = {"User-Agent":'curl/7.52.1',
