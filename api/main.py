39760c8dcc12f359d188a9bbf5b392b3c6d2540a
zhufue <499984532@qq.com>
2020-12-17 17:24:13 +0800

    提交了文件

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
+# -*- coding: utf8 -*-
 from http.server import BaseHTTPRequestHandler
-class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.end_headers()
-    self.wfile.write(self.headers.get('x-forwarded-for').encode())
-    return
+import requests
+def handler(event, context):
+    r = requests.post("https://api.github.com/repos/Zfour/my-blog-source-file/dispatches",
+    json = {"event_type": "run-it"},
+    headers = {"User-Agent":'curl/7.52.1',
+              'Content-Type': 'application/json',
+              'Accept': 'application/vnd.github.everest-preview+json',
+              'Authorization': 'token 325b0fef4d80cfb4ff06c38507d9c1f7149c3bf9'})
+    if r.status_code == 204:
+        return "This's OK!"
+    else:
+        return r.status_code
